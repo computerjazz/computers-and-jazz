6a93b490ab54bc01400461d45768c00af81f3373
computerjazz <hi@danielmerrill.com>
2021-10-21 16:51:48 -0700

    Initial Commit

---
+++ ComputersAndJazz/ComputersAndJazzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComputersAndJazzApp.swift
+//  ComputersAndJazz
+//
+//  Created by Daniel Merrill on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ComputersAndJazzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComputersAndJazz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComputersAndJazz
+//
+//  Created by Daniel Merrill on 10/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
